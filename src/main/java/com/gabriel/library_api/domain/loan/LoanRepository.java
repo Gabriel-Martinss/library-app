f0a2f771f00473ede9bc965c996d8c0ffc20bd87
Gabriel <gb.magamartins19@gmail.com>
2026-05-07 21:04:18 -0300

    feat(loan): add LoanRepository

---
+++ src/main/java/com/gabriel/library_api/domain/loan/LoanRepository.java
@@ -0,0 +1,11 @@
+package com.gabriel.library_api.domain.loan;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+       long countByUsersIdAndStatus(Long userId, LoanStatus status);
+       List<Loan> findByUserId(Long userId);
+       List<Loan> findByStatus(LoanStatus status);
+}
